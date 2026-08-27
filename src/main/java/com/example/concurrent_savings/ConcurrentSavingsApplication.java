ac6fe5a9c2e7a1daefbf83ecb7cad8bd8103ba58
hyojae02 <ansgywo0617@naver.com>
2026-08-27 15:36:37 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/example/concurrent_savings/ConcurrentSavingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrent_savings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentSavingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentSavingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concurrent_savings/ConcurrentSavingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrent_savings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentSavingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
