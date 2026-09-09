bc853fa38ce012cf9369fccbeacfb865fe6ea07c
hyojae02 <ansgywo0617@naver.com>
2026-09-09 13:45:35 +0900

    chore: 로컬 개발 인프라 설정

--- src/test/java/com/example/concurrent_savings/ConcurrentSavingsApplicationTests.java
+++ src/test/java/com/example/concurrent_savings/ConcurrentSavingsApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.concurrent_savings;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 class ConcurrentSavingsApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
